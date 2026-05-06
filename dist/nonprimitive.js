4060ca3845fa0f48d83d8c5652708e9a0b18dbf9
Nuhin <153355458+ShanjidulNuhin@users.noreply.github.com>
2026-05-06 23:01:31 +0600

    update nonprimitive

--- dist/nonprimitive.js
+++ dist/nonprimitive.js
@@ -3,4 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 let bazar = ["eggs", "milk", "bread"]; //this is an string type array
 bazar.push("butter"); //this will add butter to the bazar array
 console.log(bazar); //this will print the bazar array with the new item added
+bazar.pop(); //this will remove the last item from the bazar array which is butter
+let both = ["eggs", 123, true]; //this is an array that can contain both string, number and boolean type values
+both.push("milk"); //this will add milk to the both array
+both.push(456); //this will add 456 to the both array
+both.push(false); //this will add false to the both array
+console.log(both); //this will print the both array with the new items added
 //# sourceMappingURL=nonprimitive.js.map
