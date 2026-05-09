051c68748377a6d753673029518bcd412ac50496
Nuhin <153355458+ShanjidulNuhin@users.noreply.github.com>
2026-05-09 14:31:13 +0600

    callback function

--- src/function.ts
+++ src/function.ts
@@ -37,3 +37,14 @@ console.log(info.balance);//this will print 100
 //in this case, we have defined a method called addbalance inside the info object.
 // The addbalance method takes a number parameter called value and adds it to the balance property of the info object. 
 // It then returns the total balance after adding the value.
+
+//function in loop(callback function)
+const arr:number[]=[2,4,6,8];
+const newArr=arr.map((num:number):number=>{//map() repeat loops for every elements in every array and return a new array
+    return num*num;
+});
+console.log(newArr);//this will print [4,16,36,64]
+//in this case, we have used the map method to create a new array called newArr. 
+// The map method takes a callback function as an argument, which is executed for each element in the arr array. 
+// The callback function takes a number parameter called num and returns the result of multiplying num by itself. 
+// The newArr array will contain the results of applying the callback function to each element in the arr array.
