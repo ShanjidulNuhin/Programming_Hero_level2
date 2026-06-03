fadcdbfadbef521c7a659b9cff59900fafcd1f0f
MD. Shanjidul Islam <shanjidshanjidulislam1234@gmail.com>
2026-06-03 13:32:14 +0600

    Sprade Operators in OObjects

--- src/reasnspread.ts
+++ src/reasnspread.ts
@@ -5,3 +5,14 @@ const clgfrnds=["Rofik","shofik","tofik"];
 
 const allfriends=[...elakafriends,...schlfrnds,...clgfrnds];//this is the spread operator which is used to combine multiple arrays into one array.
 console.log(allfriends);
+
+//Spread operators in objects
+const user={ID:23,
+    name:"Shanjidul",
+    age:21
+};
+const otherInfo={Hobby: "Playing Video games",
+    freeTime:"Watching Anime"
+};
+const info={...user,...otherInfo};//this is the spread operator which is used to combine multiple objects into one object.
+console.log(info);
