adcf5c04a79a64080d64527b84f5ce4a19789811
MD. Shanjidul Islam <shanjidshanjidulislam1234@gmail.com>
2026-06-03 13:58:59 +0600

    rest operator

--- src/reasnspread.ts
+++ src/reasnspread.ts
@@ -16,3 +16,19 @@ const otherInfo={Hobby: "Playing Video games",
 };
 const info={...user,...otherInfo};//this is the spread operator which is used to combine multiple objects into one object.
 console.log(info);
+
+//Rest operators (function)
+const sendInvite=(friend1:string,freind2:string,frinde3:string)=>{
+    console.log(`invite ${friend1}, ${freind2}, ${frinde3} to the party`);
+}
+sendInvite("Rahim","Karim","Rofik");
+//Here I use arrow function
+
+//same thing with loop(forEach)
+const sendInvite2=(...friends4:string[])=>
+{
+    friends4.forEach((friends4:string)=>
+    console.log(`invite ${friends4} to the party`));
+}
+sendInvite2("Rahim","Karim","Rofik","Shofik","Tofik","Sakib","Bilu","Jobbar");
+//here i use forEach loop. now i can push as many string values as i want in the single "friends4" parameter
