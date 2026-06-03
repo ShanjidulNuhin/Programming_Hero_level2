613398417e04fa2723d189132a61db239ebb0c23
MD. Shanjidul Islam <shanjidshanjidulislam1234@gmail.com>
2026-06-03 13:19:33 +0600

    Spread operators in array

---
+++ src/reasnspread.ts
@@ -0,0 +1,7 @@
+//Spread operators in arrays
+const elakafriends=["Rahim","Karim"];
+const schlfrnds=["sakib","Bilu","jobbar"];
+const clgfrnds=["Rofik","shofik","tofik"];
+
+const allfriends=[...elakafriends,...schlfrnds,...clgfrnds];//this is the spread operator which is used to combine multiple arrays into one array.
+console.log(allfriends);
