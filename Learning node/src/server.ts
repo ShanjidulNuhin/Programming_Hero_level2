8641c53b149b4a73387d09f42b66607f542e9e62
MD. Shanjidul Islam <shanjidshanjidulislam1234@gmail.com>
2026-06-11 20:01:21 +0600

    show text in browser

--- Learning node/src/server.ts
+++ Learning node/src/server.ts
@@ -1,8 +1,15 @@
 import { createServer, IncomingMessage, Server } from "http";
 
-const server: Server = createServer((req:IncomingMessage,res)=>{
-console.log(req);
+const server: Server = createServer(
+  (req:IncomingMessage,res)=>{
+//console.log(req.url);//(/user,/products etc)
+//console.log(req.method);//(get,push,put,delete,update,post)
+if ( req.url=== "/" && req.method==="GET"){
+  res.writeHead(200,{"content-type":"text/plain"});
+  res.end("This is root rout");
+}
 })
 server.listen(5000,()=>{
   console.log("server is running");
 })
+//these are shows in terminal
