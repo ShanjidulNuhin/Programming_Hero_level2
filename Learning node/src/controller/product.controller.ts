4acb7b40a95013a5a8f861c32ef40fc9d0064008
MD. Shanjidul Islam <shanjidshanjidulislam1234@gmail.com>
2026-06-13 00:36:33 +0600

    adding controller folder

---
+++ Learning node/src/controller/product.controller.ts
@@ -0,0 +1,15 @@
+import type { IncomingMessage, ServerResponse } from "http";
+
+export const productController = (req: IncomingMessage, res: ServerResponse) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === "/products" && method === "GET") {
+        const products=[{
+            id:1,
+            name:"No. 1 product",
+        },
+    ];
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({ message: "This is Products rout", data:products }));
+    }
+}
