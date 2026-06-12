c7ed7beb16e0c70e62d42267f646bc490b4196b0
MD. Shanjidul Islam <shanjidshanjidulislam1234@gmail.com>
2026-06-13 00:37:21 +0600

    changing routes

--- Learning node/src/routes/routes.ts
+++ Learning node/src/routes/routes.ts
@@ -1,13 +1,15 @@
 import type { IncomingMessage, ServerResponse } from "http";
+import { productController } from "../controller/product.controller";
 
 export const routeHandeler=(req:IncomingMessage,res:ServerResponse)=>{
-    if ( req.url=== "/" && req.method==="GET"){
+  const url =req.url;
+  const method =req.method;
+    if ( url=== "/" && method==="GET"){
   res.writeHead(200,{"content-type":"application/json"});
   res.end(JSON.stringify({message:"This is root rout"}));
 }
-else if(req.url?.startsWith("/products")&& req.method==="GET"){
-  res.writeHead(200,{"content-type":"application/json"});
-  res.end(JSON.stringify({message:"This is Products rout"}));
+else if(url?.startsWith("/products")){
+ productController(req,res);
 }
 else{
    res.writeHead(404,{"content-type":"application/json"});
