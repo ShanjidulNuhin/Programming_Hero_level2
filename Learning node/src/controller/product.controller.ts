ec5726ec683a31546d2985a65a5bcdf01ed8ef62
MD. Shanjidul Islam <shanjidshanjidulislam1234@gmail.com>
2026-06-13 00:49:34 +0600

    Changed

--- Learning node/src/controller/product.controller.ts
+++ Learning node/src/controller/product.controller.ts
@@ -1,4 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "http";
+import { readProduct } from "../service/product.service";
 
 export const productController = (req: IncomingMessage, res: ServerResponse) => {
     const url = req.url;
@@ -9,6 +10,7 @@ export const productController = (req: IncomingMessage, res: ServerResponse) => 
             name:"No. 1 product",
         },
     ];
+    readProduct;
         res.writeHead(200, { "content-type": "application/json" });
         res.end(JSON.stringify({ message: "This is Products rout", data:products }));
     }
--- Learning node/src/service/product.service.ts
+++ Learning node/src/service/product.service.ts
@@ -1,3 +1,6 @@
+import path from "path";
+
+const filepath=path.join(process.cwd(),'./src/databse/db.json');
 export const readProduct=()=>{
-    
+    console.log(filepath);
 }
