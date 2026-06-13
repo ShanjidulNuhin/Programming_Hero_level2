82c6aa0476afccdf874ba575982316803a69b54c
MD. Shanjidul Islam <shanjidshanjidulislam1234@gmail.com>
2026-06-13 16:34:19 +0600

    changed

--- Learning node/src/controller/product.controller.ts
+++ Learning node/src/controller/product.controller.ts
@@ -84,7 +84,7 @@ export const productController = async (req: IncomingMessage, res: ServerRespons
 
         products.splice(index, 1);
         insertProduct(products);
-        sendResponse(res, 200, true, "Products deleted", null);
+        sendResponse(res, 200, true, "Products deleted",);
 
     }
 }
