4c491a139e8dc16d211b9dea12e4055d214e9851
MD. Shanjidul Islam <shanjidshanjidulislam1234@gmail.com>
2026-06-13 23:13:11 +0600

    create tables

--- express/src/server.ts
+++ express/src/server.ts
@@ -11,6 +11,29 @@ const pool =new Pool({
     connectionString:"postgresql://neondb_owner:npg_xMZvSbYmH6c8@ep-aged-scene-atsuip2g-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 })
 
+const initDB=async()=>{
+    try{
+        await pool.query(`
+           CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(20),
+            email VARCHAR(20) NOT NULL,
+            password VARCHAR(20) NOT NULL,
+            is_active BOOLEAN DEFAULT true,
+            age INT,
+
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+           ) `
+        )
+        console.log("Database created succesfully");
+    }catch(error){
+        console.log(error);
+    }
+};
+
+initDB();
+
 app.get('/', (req:Request, res:Response) => {
 //   res.send('Hello World!');
     res.status(200).json({
