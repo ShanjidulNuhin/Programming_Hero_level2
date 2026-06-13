ad9dd3431f886e7de475f9ccff1b09a1d0864d96
MD. Shanjidul Islam <shanjidshanjidulislam1234@gmail.com>
2026-06-13 12:10:22 +0600

    MOre update

--- Learning node/src/controller/product.controller.ts
+++ Learning node/src/controller/product.controller.ts
@@ -1,19 +1,32 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../service/product.service";
+import type { IProduct } from "../types/product.types";
 
 export const productController = (req: IncomingMessage, res: ServerResponse) => {
     const url = req.url;
     const method = req.method;
+
+    const urlParts = url?.split("/");
+    //console.log(urlParts);
+    const id = urlParts && urlParts[1] === 'products' ? Number(urlParts[2]) : null;
+    //console.log("This is the actual id :", id);
+
+    //get all products
     if (url === "/products" && method === "GET") {
-    //     const products=[{
-    //         id:1,
-    //         name:"No. 1 product",
-    //     },
-    // ];
-    const products =readProduct();
+        //     const products=[{
+        //         id:1,
+        //         name:"No. 1 product",
+        //     },
+        // ];
+        const products = readProduct();
         res.writeHead(200, { "content-type": "application/json" });
-        res.end(JSON.stringify({ 
-            message: "This is Products rout", data:products})
+        res.end(JSON.stringify({
+            message: "This is Products rout", data: products
+        })
         );
+    }else if(method==="GET"&&id!==null){
+                const products = readProduct();
+const product = products.find((p:IProduct)=>p.id===id);
+console.log(product);
     }
 }
