c402bc2950878726834c28a0a1ff152dfdb0fb03
MD. Shanjidul Islam <shanjidshanjidulislam1234@gmail.com>
2026-06-29 21:42:04 +0600

    module 9-1

--- express/src/app.ts
+++ express/src/app.ts
@@ -4,11 +4,22 @@ import { initDB, pool } from "./db";
 import { userRout } from "./modules/user/user.route";
 import { profileRoute } from "./modules/profile/profile.route";
 import { authRoute } from "./modules/auth/auth.route";
+import fs from "fs";
+
 const app : Application = express()
 
 app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({extended:true}))
+
+app.use((req, res, next) => {
+  console.log('method - URL - Time:',req.method, req.url ,Date.now());
+  const log =`\nMethod -> ${req.method} - Time -> ${Date.now()} - URL -> ${req.url}\n`;
+  fs.appendFile('loger.txt',log,(err)=>{
+    console.log(err);
+  });
+  next();
+});
 
 app.get('/', (req:Request, res:Response) => {
     res.status(200).json({
